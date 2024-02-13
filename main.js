8a8f74e23a5bc020713c35f2e0affaa04106b18c
atikurahad <atikurahad@gmail.com>
2024-02-13 10:23:31 +0600

    server create and run

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
+// request response in node js
 
-const http = require('http')
+ const http = require('http')
 
-const server = http.createServer(  function(req,res){
-    res.end("hello nodeJs")
+ const server = http.createServer((req,res)=>{
+if(req.url == '/'){
+  res.writeHead(200,{'content-type':'text/html'} )
+  res.write('<h1> I am from Home</h1>')
+  res.end()
 }
+  else if (req.url == '/about'){
+    res.writeHead(200,{'content-type':'text/html'} )
+    res.write('<h1> I am from About</h1>')
+    res.end()
+  }
+  else if (req.url == '/contact'){
+    res.writeHead(200,{'content-type':'text/html'} )
+    res.write('<h1> I am from Contact</h1>')
+    res.end()
+  }
 
-)
-server.listen(5050)
-console.log("server is listening on port 5050")
+  else{
+    res.writeHead(404,{'content-type':'text/html'} )
+    res.write('<h1> 404 not found</h1>')
+    res.end()
+  }
+
+ })
+
+ server.listen(5050)
+ console.log("Server is running on port 5050")
